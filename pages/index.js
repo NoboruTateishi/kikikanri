597b9031ce3faedd59b04b44447f15d0e4ef1064
NoboruTateishi <tatengine@gmail.com>
2025-05-12 05:51:00 +0900

    Add pages directory

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+// pages/index.js (Next.js 12まで or React + CRA 構成)
+import MedicalDeviceManager from "../MedicalDeviceManager";
+
+export default function Home() {
+  return <MedicalDeviceManager />;
+}
