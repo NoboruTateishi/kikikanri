5a3711f42ecd6d84e4d7dcf2d3799acdde32722e
NoboruTateishi <tatengine@gmail.com>
2025-05-12 05:25:47 +0900

    初回

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaF9dRPtufaOqqK9-IT1vumFrsPwr5ypc",
+  authDomain: "kiki-50e6a.firebaseapp.com",
+  projectId: "kiki-50e6a",
+  storageBucket: "kiki-50e6a.firebasestorage.app",
+  messagingSenderId: "6456636660",
+  appId: "1:6456636660:web:5cdb25135876b3dbf2daa1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
